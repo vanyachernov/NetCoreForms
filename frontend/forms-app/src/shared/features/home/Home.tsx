d013f6922e113b7b033e6866b36f941e7917b708
Vanya Chernov <vanyachernov2001@gmail.com>
2024-10-24 23:04:23 +0300

    Update bundled v1.2

--- frontend/forms-app/src/shared/features/home/Home.tsx
+++ frontend/forms-app/src/shared/features/home/Home.tsx
@@ -1,7 +1,7 @@
 import {useEffect, useState} from 'react';
 import {Image, Pane} from "evergreen-ui";
 import Greetings from "../../../components/Greetings.tsx";
-import greetings from "./public/assets/images/greetings_bg.png";
+import greetings from "../../../../public/assets/images/greetings_bg.png";
 
 const Home = () => {
     const [isMobile, setIsMobile] =
